871d99e82dcbfddd9f7805508abeb3d52f47ad97
Rahmatullo <rahmatulloabdusamadov167@gmail.com>
2024-07-12 20:49:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+// home work 7
+// function test(n) {
+//   let cnt=1;
+//   for(let i=1;i<=n;i++){
+//     cnt*=i;
+//   }
+//   return cnt;
+// }
+//   console.log(test(4)); 
+
+// task2
+// function test(n) {
+//   let cnt=1;
+//   for(let i=1;i<=n;i++){
+//     cnt+=i;
+//   }
+//   return cnt;
+// }
+//   console.log(test(5)); 
+
+// task3
+// function test(a,b){
+//     return a**b
+// }
+// console.log(test(2,3));
+
+// task5
+// function test(a){
+// let sum=0;
+// return (b) =>{
+//     sum=a+b;
+//     return sum;
+// }
+// }
+// let ans= test(5);
+// console.log(ans(3));
+
+// task4
